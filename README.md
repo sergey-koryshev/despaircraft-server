007f9a5a3989f6aa20bc38a6742953ee44cc2648
Sergey Koryshev <sergey.koryshev@outlook.com>
2025-07-16 08:19:41 +0200

    Updated Dockerfile and entrypoint script for improved RCON handling and user permissions

--- README.md
+++ README.md
@@ -2,7 +2,17 @@
 
 This repository contains Dockerfile to run pure Fabric Minecraft server in Docker.
 
-## Docker container shutdown
+## Building Arguments
+
+The docker file have a few arguments:
+
+| Name | Required | Description |
+| - | - | - |
+| **FABRIC_SERVER_DOWNLOAD_URL** | Yes | URL to download fabric server |
+| **UID** | No | id of user (default value is `25575`) |
+| **GID** | No | id of group (default value is `25575`) |
+
+## Docker Container Shutdown
 
 The server is configured to be gracefully exited on container shutdown process by tool [mcrcon by Tiiffi](https://github.com/Tiiffi/mcrcon), if you map your own `server.properties`, make sure the file has the following properties set:
 
@@ -13,7 +23,11 @@ rcon.password=SomePassword
 
 or ensure that the file is available for writing and the properties will be set automatically (in this case password for rcon will be `RconPassword12345`).
 
-## Starting container
+## Docker Container User
+
+The minecraft server is running under non-root user `minecraft_user` (group `minecraft_group`) with prepopulated `UID` and `GID` (`25575` by default for both). If you mount any directories into the container, make sure the user has access to them.
+
+## Starting Container
 
 Container can be started by the following command:
 
@@ -25,10 +39,10 @@ Note, that `--init` parameter is desirable, but not required. Container may work
 
 ### Customization
 
-You can specify memory limits for running Minecraft server:
+You can use the following environment variables to adjust execution of the server:
 
-```bash
-docker run ... -e XMX=5G -e XMS=5G ...
-```
-
-Default values for both parameters is `2G`
+| Name | Default Value | Description |
+| - | - | - |
+| XMS | 2G | Initial size of the memory allocation pool  |
+| XMX | 2G | Maximum size of the memory allocation pool |
+| RCONPASSWORD | Random value (exposed in logs) | RCON's password |
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,41 +1,72 @@
 #!/bin/bash
+set -e
 
 XMX=${XMX:-2G}
 XMS=${XMS:-2G}
-rconDefaultPassword="RconPassword12345"
+RCONPASSWORD=${RCONPASSWORD}
+
 enableRconPropertyName="enable-rcon"
 rconPasswordPropertyName="rcon\.password"
 rconPortPropertyName="rcon\.port"
 
 cleanup() { 
-  echo "Received exiting signal"
+  echo "Received exiting signal: $1"
 
   if [ "$(getServerPropertyValue "$enableRconPropertyName")" == "true" ] && [ "$(getServerPropertyValue "$rconPasswordPropertyName")" != "" ]; then
-    echo "Server will be stopped by mcrcon tool..."
-    exec /usr/local/bin/mcrcon/mcrcon -H localhost -P "$(getServerPropertyValue $rconPortPropertyName)" -p "$(getServerPropertyValue "$rconPasswordPropertyName")" "say Server is shutting down..." save-all stop
+    if pidof java > /dev/null; then
+      echo "Server will be stopped by mcrcon tool..."
+      exec /usr/local/bin/mcrcon/mcrcon -H localhost -P "$(getServerPropertyValue "$rconPortPropertyName")" -p "$(getServerPropertyValue "$rconPasswordPropertyName")" "say Server is shutting down..." save-all stop || echo "Warning: mcrcon failed to stop server gracefully"
+    else
+      echo "Server is not running, exiting..."
+      exit 0
+    fi
   fi
 }
 
 getServerPropertyValue() {
-  echo "$(sed -nE 's/^'"$1"'=(.*)$/\1/p' server.properties)"
+  echo "$(sed -nE 's/^'"$1"'=(.*)$/\1/p' /minecraft/server.properties)"
 }
 
 setServerPropertyValue() {
-  sed server.properties -e '/^'"$1"'/s/=.*$/='"$2"'/' > /tmp/modified_properties && cp /tmp/modified_properties server.properties
+  local key="$1"
+  local value="$2"
+  local file="/minecraft/server.properties"
+  local tmpfile="/minecraft/server.properties.tmp"
+
+  if grep -q "^$key=" "$file"; then
+    echo "Setting '$key' in '$file'"
+    sed "$file" -e "/^$key/s/=.*$/=$value/" > "$tmpfile" && mv "$tmpfile" "$file"
+  else
+    echo "Adding '$key' to '$file'"
+    echo "$key=$value" >> "$file"
+  fi
+
+  chmod 755 "$file"
 }
 
-trap cleanup EXIT SIGINT SIGTERM
+trap 'cleanup EXIT' EXIT
+trap 'cleanup SIGINT' SIGINT
+trap 'cleanup SIGTERM' SIGTERM
 
 if [ "$(getServerPropertyValue "$enableRconPropertyName")" == "false" ]; then
-  echo "Enabling rcon..."
+  echo "RCON is disabled in server.properties, enabling it..."
   setServerPropertyValue "$enableRconPropertyName" "true"
 fi
 
 if [ "$(getServerPropertyValue "$rconPasswordPropertyName")" == "" ]; then
-  echo "Rcon password is empty, setting default one..."
-  setServerPropertyValue "$rconPasswordPropertyName" "$rconDefaultPassword"
+  echo "RCON password is empty in server.properties"
+
+  if [ -z "$RCONPASSWORD" ]; then
+    RCONPASSWORD=$(openssl rand -base64 12)
+    echo "Using random RCON password: $RCONPASSWORD"
+  else
+    echo "Using provided RCON password"
+  fi
+
+  setServerPropertyValue "$rconPasswordPropertyName" "$RCONPASSWORD"
 fi
 
+echo "Starting Minecraft server at $(date)..."
 java -Xmx"$XMX" -Xms"$XMS" -jar minecraft_server.jar --nogui &
 
 PID=$(pidof java)
